41c129a634ac91c0f939e9756a70b9dd605d1145
Shontymohite <94635317+Shontymohite@users.noreply.github.com>
2022-04-19 23:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.querySelectorAll("button")[0].addEventListener("click", crash);
 
 function crash() {
-    var audio = new Audio("sounds/crash.mp3");
+    var audio = new Audio("crash.mp3");
     audio.play();
 }
 
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyW') {
-        var audio = new Audio("sounds/crash.mp3");
+        var audio = new Audio("crash.mp3");
         audio.play();
 
     }
@@ -19,14 +19,14 @@ document.addEventListener("keydown", function(event) {
 document.querySelectorAll("button")[1].addEventListener("click", kick);
 
 function kick() {
-    var audio = new Audio("sounds/kick-bass.mp3");
+    var audio = new Audio("kick-bass.mp3");
     audio.play();
 }
 
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyA') {
-        var audio = new Audio("sounds/kick-bass.mp3");
+        var audio = new Audio("kick-bass.mp3");
         audio.play();
 
     }
@@ -39,13 +39,13 @@ document.addEventListener("keydown", function(event) {
 document.querySelectorAll("button")[2].addEventListener("click", snare);
 
 function snare() {
-    var audio = new Audio("sounds/snare.mp3");
+    var audio = new Audio("snare.mp3");
     audio.play();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyS') {
-        var audio = new Audio("sounds/snare.mp3");
+        var audio = new Audio("snare.mp3");
         audio.play();
 
     }
@@ -57,13 +57,13 @@ document.addEventListener("keydown", function(event) {
 document.querySelectorAll("button")[3].addEventListener("click", tom1);
 
 function tom1() {
-    var audio = new Audio("sounds/tom-1.mp3");
+    var audio = new Audio("tom-1.mp3");
     audio.play();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyD') {
-        var audio = new Audio("sounds/tom-1.mp3");
+        var audio = new Audio("tom-1.mp3");
         audio.play();
 
     }
@@ -71,20 +71,20 @@ document.addEventListener("keydown", function(event) {
 
 
 function tom1() {
-    var audio = new Audio("sounds/tom-1.mp3");
+    var audio = new Audio("tom-1.mp3");
     audio.play();
 }
 
 document.querySelectorAll("button")[4].addEventListener("click", tom2);
 
 function tom2() {
-    var audio = new Audio("sounds/tom-2.mp3");
+    var audio = new Audio("tom-2.mp3");
     audio.play();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyJ') {
-        var audio = new Audio("sounds/tom-2.mp3");
+        var audio = new Audio("tom-2.mp3");
         audio.play();
 
     }
@@ -98,12 +98,12 @@ document.addEventListener("keydown", function(event) {
 document.querySelectorAll("button")[5].addEventListener("click", tom3);
 
 function tom3() {
-    var audio = new Audio("sounds/tom-3.mp3");
+    var audio = new Audio("tom-3.mp3");
     audio.play();
 }
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyK') {
-        var audio = new Audio("sounds/tom-3.mp3");
+        var audio = new Audio("tom-3.mp3");
         audio.play();
 
     }
@@ -112,13 +112,13 @@ document.addEventListener("keydown", function(event) {
 document.querySelectorAll("button")[6].addEventListener("click", tom4);
 
 function tom4() {
-    var audio = new Audio("sounds/tom-4.mp3");
+    var audio = new Audio("tom-4.mp3");
     audio.play();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.code == 'KeyH') {
-        var audio = new Audio("sounds/tom-4.mp3");
+        var audio = new Audio("tom-4.mp3");
         audio.play();
 
     }
